b725a9b37535a513b03bab5337aaae809d8bd734
Chiemerie <alexemerie7@gmail.com>
2022-05-16 10:29:05 +0300

    Added auth modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,26 @@ $(() => {
     const year = new Date().getFullYear();
     const copyrightYearElement = $("#copyright-year");
     copyrightYearElement.text(year);
+
+//    Live Auction Timer
+    const timer = $("#live-auction-timer");
+    const endTime = new Date();
+    endTime.setTime(endTime.getTime() + (120 * 60 * 1000));
+    setInterval(() => {
+        const now = new Date().getTime();
+        const timeLeft = endTime.getTime() - now;
+
+        // const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        timer.text(
+            "Time left: "
+            + String(hours).padStart(2, '0')
+            + ":"
+            + String(minutes).padStart(2, '0')
+            + ":"
+            + String(seconds).padStart(2, '0')
+        );
+    }, 1000);
 });
